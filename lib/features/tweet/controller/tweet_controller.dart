c9eaf284c6555a6f79b9ce8dff12d664b85d45d5
pulkit-singhall <singhal.pulkit667@gmail.com>
2023-11-01 01:25:01 +0530

    Tweet Model

--- lib/features/tweet/controller/tweet_controller.dart
+++ lib/features/tweet/controller/tweet_controller.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/models/tweet_model.dart';
 
 class TweetController extends StateNotifier<bool>{
   TweetController() : super(false);
 
   // tweet share action
-  void shareTweet({required String text, required BuildContext context}){
+  void shareTweet({required TweetModel tweet, required BuildContext context}){
 
   }
 
   // getting hashtags from the tweet
-  List<String> _getHashTagFromTweet (String tweet){
+  List<String> _getHashTagFromTweet (String tweetText){
     List<String> hashTags = [];
-    List<String> words = tweet.split(' ');
+    List<String> words = tweetText.split(' ');
 
     for(String word in words){
       if(word.startsWith('#')){
@@ -23,8 +24,8 @@ class TweetController extends StateNotifier<bool>{
   }
 
   // getting links from the tweet
-  List<String> _getLinkFromTweet (String tweet){
-    List<String> words = tweet.split(' ');
+  List<String> _getLinkFromTweet (String tweetText){
+    List<String> words = tweetText.split(' ');
     List<String> links = [];
     for(String word in words){
       if(word.startsWith('https://') || word.startsWith('www.')){
--- lib/models/tweet_model.dart
+++ lib/models/tweet_model.dart
@@ -1,3 +1,83 @@
-class TweetModel{
+import 'package:flutter/cupertino.dart';
 
+@immutable
+class TweetModel {
+  final String text;
+  final List<String> hashtags;
+  final List<String> links;
+  final String userId;
+  final String tweetId;
+  final DateTime tweetTime;
+  final List<String> likes;
+  final List<String> comments;
+  final int reshareCount;
+
+  const TweetModel(
+      {required this.likes,
+      required this.comments,
+      required this.hashtags,
+      required this.links,
+      required this.userId,
+      required this.tweetId,
+      required this.tweetTime,
+      required this.text,
+      required this.reshareCount});
+
+  TweetModel copyWith({
+    String? text,
+    List<String>? hashtags,
+    List<String>? links,
+    String? userId,
+    DateTime? tweetTime,
+    List<String>? likes,
+    List<String>? comments,
+    String? tweetId,
+    int? reshareCount,
+  }) {
+    return TweetModel(
+      text: text ?? this.text,
+      hashtags: hashtags ?? this.hashtags,
+      links: links ?? this.links,
+      userId: userId ?? this.userId,
+      tweetTime: tweetTime ?? this.tweetTime,
+      likes: likes ?? this.likes,
+      comments: comments ?? this.comments,
+      tweetId: tweetId ?? this.tweetId,
+      reshareCount: reshareCount ?? this.reshareCount,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+
+    result.addAll({'text': text});
+    result.addAll({'hashtags': hashtags});
+    result.addAll({'links': links});
+    result.addAll({'userId': userId});
+    result.addAll({'tweetTime': tweetTime.millisecondsSinceEpoch});
+    result.addAll({'likes': likes});
+    result.addAll({'comments': comments});
+    result.addAll({'reshareCount': reshareCount});
+
+    return result;
+  }
+
+  factory TweetModel.fromMap(Map<String, dynamic> map) {
+    return TweetModel(
+      text: map['text'] ?? '',
+      hashtags: List<String>.from(map['hashtags']),
+      links: List<String>.from(map['links']),
+      userId: map['userId'] ?? '',
+      tweetTime: DateTime.fromMillisecondsSinceEpoch(map['tweetTime']),
+      likes: List<String>.from(map['likes']),
+      comments: List<String>.from(map['comments']),
+      tweetId: map['\$id'] ?? '',
+      reshareCount: map['reshareCount']?.toInt() ?? 0,
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Tweet(text: $text, hashtags: $hashtags, links: $links, userId: $userId, tweetTime: $tweetTime, likes: $likes, comments: $comments, tweetId: $tweetId, reshareCount: $reshareCount)';
+  }
 }
