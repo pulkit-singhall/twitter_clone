24056c1069d6268756005243d5ce094eb5f925b7
pulkit-singhall <singhal.pulkit667@gmail.com>
2023-11-19 01:03:15 +0530

    Null Error Fix

--- lib/features/home/ui/homeUI.dart
+++ lib/features/home/ui/homeUI.dart
@@ -27,182 +27,195 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final UserModel currentUserModel =
-        ref.watch(currentUserModelProvider).value!;
-    final String profilePic = currentUserModel.profilePic;
-    final String name = currentUserModel.name;
-    final int followers = currentUserModel.followers.length;
-    final int following = currentUserModel.following.length;
+    final currentUserModel =
+        ref.watch(currentUserModelProvider);
 
-    return Scaffold(
-      appBar: UICommon.reusableAppBar(),
-      body: IndexedStack(
-        index: currentPage,
-        children: homeWidgets,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // tweet screen navigation
-          Navigator.push(context, Routes.newTweetRoute());
-        },
-        backgroundColor: Pallete.blueColor,
-        elevation: 3,
-        child: const Icon(
-          Icons.add,
-          color: Pallete.whiteColor,
-          size: 35,
-        ),
-      ),
-      drawer: Drawer(
-        backgroundColor: Pallete.backgroundColor,
-        elevation: 3,
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(14.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                GestureDetector(
-                  child: CircleAvatar(
-                    backgroundImage: NetworkImage(profilePic),
-                    radius: 25,
-                  ),
-                  onTap: () {
-                    // navigate to profile page
-                    Navigator.push(context, Routes.profilePageRoute());
-                  },
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                Text(
-                  name,
-                  style: const TextStyle(
-                      color: Pallete.whiteColor,
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(
-                  height: 3,
-                ),
-                Text(
-                  '@$name',
-                  style:
-                      const TextStyle(color: Pallete.greyColor, fontSize: 18),
-                ),
-                const SizedBox(
-                  height: 18,
-                ),
-                Row(
-                  children: [
-                    Text(
-                      following.toString(),
-                      style: const TextStyle(
-                          color: Pallete.whiteColor,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20),
-                    ),
-                    const SizedBox(
-                      width: 4,
-                    ),
-                    const Text(
-                      'Following',
-                      style: TextStyle(color: Pallete.greyColor, fontSize: 18),
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      followers.toString(),
-                      style: const TextStyle(
-                          color: Pallete.whiteColor,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20),
-                    ),
-                    const SizedBox(
-                      width: 4,
-                    ),
-                    const Text(
-                      'Followers',
-                      style: TextStyle(color: Pallete.greyColor, fontSize: 18),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                GestureDetector(
-                  child: const Text(
-                    'Profile',
-                    style: TextStyle(
-                        color: Pallete.whiteColor,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 24),
-                  ),
-                  onTap: () {
-                    Navigator.push(context, Routes.profilePageRoute());
-                  },
-                ),
-                const Spacer(),
-                Center(
-                    child: ElevatedButton(
-                        onPressed: () {
-                          // logout action
-                          // here
-                          Navigator.pushReplacement(context, Routes.signUpRoute());
-                        },
-                        child: const Text(
-                          'Logout',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 18),
-                        )))
-              ],
+    Widget homeUI = currentUserModel.when(data: (userModel){
+      if(userModel != null){
+        final String profilePic = userModel.profilePic;
+        final String name = userModel.name;
+        final int followers = userModel.followers.length;
+        final int following = userModel.following.length;
+        return Scaffold(
+          appBar: UICommon.reusableAppBar(),
+          body: IndexedStack(
+            index: currentPage,
+            children: homeWidgets,
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              // tweet screen navigation
+              Navigator.push(context, Routes.newTweetRoute());
+            },
+            backgroundColor: Pallete.blueColor,
+            elevation: 3,
+            child: const Icon(
+              Icons.add,
+              color: Pallete.whiteColor,
+              size: 35,
             ),
           ),
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Pallete.backgroundColor,
-        type: BottomNavigationBarType.fixed,
-        showUnselectedLabels: false,
-        showSelectedLabels: false,
-        currentIndex: currentPage,
-        onTap: (value) {
-          setState(() {
-            currentPage = value;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: BottomItem(
-                path: currentPage == 0
-                    ? AssetsConstants.homeFilledIcon
-                    : AssetsConstants.homeOutlinedIcon,
-                height: 30,
-                width: 30,
-                color: Pallete.whiteColor),
-            label: 'Home',
+          drawer: Drawer(
+            backgroundColor: Pallete.backgroundColor,
+            elevation: 3,
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(14.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    GestureDetector(
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(profilePic),
+                        radius: 25,
+                      ),
+                      onTap: () {
+                        // navigate to profile page
+                        Navigator.push(context, Routes.profilePageRoute());
+                      },
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      name,
+                      style: const TextStyle(
+                          color: Pallete.whiteColor,
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 3,
+                    ),
+                    Text(
+                      '@$name',
+                      style:
+                      const TextStyle(color: Pallete.greyColor, fontSize: 18),
+                    ),
+                    const SizedBox(
+                      height: 18,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          following.toString(),
+                          style: const TextStyle(
+                              color: Pallete.whiteColor,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20),
+                        ),
+                        const SizedBox(
+                          width: 4,
+                        ),
+                        const Text(
+                          'Following',
+                          style: TextStyle(color: Pallete.greyColor, fontSize: 18),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          followers.toString(),
+                          style: const TextStyle(
+                              color: Pallete.whiteColor,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20),
+                        ),
+                        const SizedBox(
+                          width: 4,
+                        ),
+                        const Text(
+                          'Followers',
+                          style: TextStyle(color: Pallete.greyColor, fontSize: 18),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    GestureDetector(
+                      child: const Text(
+                        'Profile',
+                        style: TextStyle(
+                            color: Pallete.whiteColor,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24),
+                      ),
+                      onTap: () {
+                        Navigator.push(context, Routes.profilePageRoute());
+                      },
+                    ),
+                    const Spacer(),
+                    Center(
+                        child: ElevatedButton(
+                            onPressed: () {
+                              // logout action
+                              // here
+                              Navigator.pushReplacement(context, Routes.signUpRoute());
+                            },
+                            child: const Text(
+                              'Logout',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 18),
+                            )))
+                  ],
+                ),
+              ),
+            ),
           ),
-          BottomNavigationBarItem(
-              icon: BottomItem(
-                  path: currentPage == 1
-                      ? AssetsConstants.searchIcon
-                      : AssetsConstants.searchIcon,
-                  height: 30,
-                  width: 30,
-                  color: Pallete.whiteColor),
-              label: 'Search'),
-          BottomNavigationBarItem(
-              icon: BottomItem(
-                  path: currentPage == 2
-                      ? AssetsConstants.notificationFilledIcon
-                      : AssetsConstants.notificationOutlinedIcon,
-                  height: 30,
-                  width: 30,
-                  color: Pallete.whiteColor),
-              label: 'Notification'),
-        ],
-      ),
-    );
+          bottomNavigationBar: BottomNavigationBar(
+            backgroundColor: Pallete.backgroundColor,
+            type: BottomNavigationBarType.fixed,
+            showUnselectedLabels: false,
+            showSelectedLabels: false,
+            currentIndex: currentPage,
+            onTap: (value) {
+              setState(() {
+                currentPage = value;
+              });
+            },
+            items: [
+              BottomNavigationBarItem(
+                icon: BottomItem(
+                    path: currentPage == 0
+                        ? AssetsConstants.homeFilledIcon
+                        : AssetsConstants.homeOutlinedIcon,
+                    height: 30,
+                    width: 30,
+                    color: Pallete.whiteColor),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                  icon: BottomItem(
+                      path: currentPage == 1
+                          ? AssetsConstants.searchIcon
+                          : AssetsConstants.searchIcon,
+                      height: 30,
+                      width: 30,
+                      color: Pallete.whiteColor),
+                  label: 'Search'),
+              BottomNavigationBarItem(
+                  icon: BottomItem(
+                      path: currentPage == 2
+                          ? AssetsConstants.notificationFilledIcon
+                          : AssetsConstants.notificationOutlinedIcon,
+                      height: 30,
+                      width: 30,
+                      color: Pallete.whiteColor),
+                  label: 'Notification'),
+            ],
+          ),
+        );
+      }
+      else{
+        return UICommon.progressIndicator();
+      }
+    }, error: (e,st){
+      return Center(child: Text(e.toString()),);
+    }, loading: (){
+      return UICommon.progressIndicator();
+    });
+
+    return homeUI;
   }
 }
